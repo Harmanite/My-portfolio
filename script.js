074a3a4469781e847ea72a1fa95bde5134d052c7
Harmanite <philipharman96@gmail.com>
2024-08-14 08:34:13 +0300

    My portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const mobileMenu = document.querySelector(".mobile-menu");
+    const mobileNavIcon = document.querySelector(".mobile-nav-icon");
+    mobileMenu.classList.toggle("open");
+    mobileNavIcon.classList.toggle("open");
+}
